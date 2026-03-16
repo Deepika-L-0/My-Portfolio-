7cfabc194ebf8b2e75a2c2019d6198c7fc25e84e
Deepika <aishudeepu2005@gmail.com>
2026-03-16 15:21:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var arrayNames = ["riddhi","divya","sangeetha"];
+{
+arrayNames.push("shalini");
+console.log(arrayNames);
+}
+
+
+var arrayNames = ["riddhi","divya","sangeetha"];
+{
+    arrayNames.pop("divya")
+    console.log(arrayNames);
+   
+}
+
+var arrayNames = ["riddhi","divya","sangeetha"];
+{
+arrayNames.unshift("koki")
+console.log(arrayNames);
+}
+
+var arrayNames = ["riddhi","divya","sangeetha"];
+{
+    arrayNames.slice(0,2)
+    console.log(arrayNames);
+}
+
+var arrayNames = ["riddhi","divya","sangeetha"];
+{
+    arrayNames.shift("aishu")
+    console.log(arrayNames);
+}
